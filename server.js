858e683d396a03193bb8efadf42ec6c85d136127
Anup mishra <40824863+Anupm0@users.noreply.github.com>
2025-12-12 07:30:36 +0530

    Simplify CORS configuration in server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
