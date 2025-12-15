01012e65e3e8e64aee1bcc5f3da692709dcf48df
AntiMatter <varunbhole@gmail.com>
2025-12-15 23:45:15 +0530

    New Endpointss

--- server.js
+++ server.js
@@ -49,10 +49,14 @@ const corsOptions = {
 };
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  const origin = req.headers.origin;
+  const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173', 'https://liquidata.dev', 'https://www.liquidata.dev'];
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
